fd517ca39288e050cc0465391de4d071aa704125
Pamood <pamoodjayaratne2001@gmail.com>
2024-05-17 15:14:50 +0530

    chore: Update styling for Navbar and Welcome page

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,20 @@
-import MapView from "./MapView";
-import UserDetails from "./userDetails";
+import MapView from "./MapView"
+import UserPic from "./UserPic"
+import UserDetails from "./UserDetails"
+import TabsDemo from "./TabsDemo"
 
 function LandingPage() {
   return (
     <>
-      <MapView />
-      <UserDetails />
+      <div className="flex flex-col items-center min-h-screen">
+        <div className="flex items-center space-x-2">
+          <UserPic />
+          <UserDetails />
+        </div>
+        <TabsDemo />
+      </div>
     </>
-  );
+  )
 }
 
-export default LandingPage;
+export default LandingPage
