e70d7e03c212f483e56ab0332a6469ecfd96070a
Kavishcan <kavishcan2002@gmail.com>
2024-05-17 12:45:20 +0530

    chore: Update npm dependencies and add Kinde authentication components to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
-import LandingPage from "./pages/LandingPage/LandingPage"
-import Dashboard from "./pages/Dashboard/Dashboard"
-import { Button } from "@/components/ui/button"
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import { Button } from "@/components/ui/button";
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 
 const Navbar = () => {
+  const { login, register, logout } = useKindeAuth();
+  const { user } = useKindeAuth();
+  const isAdmin = user?.email === "kavishcanvsk@gmail.com";
+  console.log(isAdmin);
   return (
     <nav className="p-3 flex items-center justify-between">
       <img src="/logo.png" alt="Logo" className="h-12 mr-4 ml-6" />{" "}
@@ -15,29 +21,55 @@ const Navbar = () => {
           </Link>
         </li>
         <li>
-          <Link
-            to="/dashboard"
-            className="hover:text-gray-200 transition duration-200"
-          >
-            Dashboard
-          </Link>
+          {isAdmin ? (
+            <Link
+              to="/dashboard"
+              className="hover:text-gray-200 transition duration-200"
+            >
+              Dashboard
+            </Link>
+          ) : (
+            <></>
+          )}
         </li>
       </ul>
-      <Button variant="default">Sign In</Button>
+      <div className="gap-10">
+        {!user && (
+          <Button variant="default" onClick={register} type="button">
+            Sign up
+          </Button>
+        )}
+        {!user ? (
+          <Button variant="default" onClick={login} type="button">
+            Sign in
+          </Button>
+        ) : (
+          <Button variant="default" onClick={logout} type="button">
+            Sign out
+          </Button>
+        )}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
-  )
+    <KindeProvider
+      clientId="cae1ce5b0350465cb66e2def398373ca"
+      domain="https://jimmy2002.kinde.com"
+      redirectUri="http://localhost:5173"
+      logoutUri="http://localhost:5173"
+    >
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </KindeProvider>
+  );
 }
 
-export default App
+export default App;
