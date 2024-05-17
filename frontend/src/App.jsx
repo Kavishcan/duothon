027041e32e7f1561414e5032f8d58d738ae262cb
Pamood <pamoodjayaratne2001@gmail.com>
2024-05-17 10:48:05 +0530

    sss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,25 @@
-import React from "react"
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
 import LandingPage from "./pages/LandingPage/LandingPage"
 import Dashboard from "./pages/Dashboard/Dashboard"
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
+    <nav className=" p-3 flex items-center">
+      <img src="/logo.png" alt="Logo" className="h-12 mr-4 ml-6" />{" "}
+      {/* adjust size as needed */}
+      <ul className="flex justify-around text-slate-600 text-lg">
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" className="hover:text-gray-200 transition duration-200">
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/dashboard">Dashboard</Link>
+          <Link
+            to="/dashboard"
+            className="hover:text-gray-200 transition duration-200"
+          >
+            Dashboard
+          </Link>
         </li>
       </ul>
     </nav>
